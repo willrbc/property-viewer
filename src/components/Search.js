b5a05effc15273656f595fd062c85e144314f5f2
Will Carter <will.carter@me.com>
2018-03-04 13:30:15 +0000

    Results filtering but not showing

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,25 +10,40 @@ class Loading extends Component {
   }
 }
 
+class Results extends Component {
+  render() {
+    if (this.props.results.length > 0){
+      console.log('rendering results')
+      return (
+        <ul>
+          {this.props.results.map((result)=>{
+            <li>{result}</li>
+          })}
+        </ul>
+      )
+    } else {
+      return null
+    }
+  }
+}
+
 class Search extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log(nextProps.results)
+    return true
     return this.props.loading !== nextProps.loading
   }
 
   render() {
     const props = this.props
-    let Results = props.results.length > 0 ? props.results.map((result)=>{
-      return (<div>{result}</div>)
-    }) : ()=>{return null};
     return (
     <div>
       <div className={props.classes}>
         <input type='text' className={props.inputClasses} onChange={props.onSearchChange} placeholder={props.placeholder}/>
       </div>
-      <Loading loading={props.loading}/>
-      <Results/>
+      {/* <Loading loading={props.loading}/> */}
+      <Results results={props.results}/>
     </div>
     )
   }
