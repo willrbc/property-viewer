1933ad09c7d53a54df4d77ddc8e49e7b72166157
Will Carter <will.carter@me.com>
2018-03-04 11:54:01 +0000

    remove autocomplete

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-import { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
-
-import SearchComponent from './PlacesAutocomplete'
-
-
-class SimpleForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { address: '' }
-    this.onChange = (address) => this.setState({ address })
-  }
-  handleFormSubmit = (event) => {
-    event.preventDefault()
-    geocodeByAddress(this.state.address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error))
-  }
-
-  render() {
-    const inputProps = {
-      value: this.state.address,
-      onChange: this.onChange,
-      placeholder: 'Enter location here'
-    }
-
-
-    return (
-      <form onSubmit={this.handleFormSubmit}>
-        <SearchComponent inputProps={inputProps} />
-        <button type="submit">Submit</button>
-      </form>
-    )
-  }
-}
-
+import Search from './components/Search'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {searchTerm:''}
+  }
   render() {
     return (<div className="App">
-      <SimpleForm />
+      <Search />
     </div>)
   }
 }
 
-export default App;
+export default App
